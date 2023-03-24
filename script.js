b98ec4ba3330e1ec2f39309249fb58c8df545bee
indira-faizullina <indiramus@list.ru>
2023-03-25 00:15:36 +1100

    Added modal window

--- script.js
+++ script.js
@@ -1,107 +1,138 @@
-const skillsArr = [
+const skillsArray = [
   {
     id: '1665474698659',
-    skillDate: '05.01.2022',
+    skillDate: '2022-01-05',
     skillName: 'Первая улыбка',
   },
   {
     id: '1665474725035',
-    skillDate: '01.10.2022',
+    skillDate: '2022-10-01',
     skillName: 'Первые шаги',
   },
 ]
 
-// addEventListener to input
+const containerNode = document.querySelector('.container')
+const formNode = containerNode.querySelector('#add-form')
+const newSkillInputNode = formNode.querySelector('#new-skill')
+const dateInputNode = formNode.querySelector('.input-date')
+const submitButtonNode = formNode.querySelector('.submit-button')
+const tableNode = containerNode.querySelector('table')
 
-const newSkillInputForm = document.querySelector('form')
-newSkillInputForm.addEventListener('submit', (event) => {
+// слушатель на кнопку добавления нового навыка + валидация
+submitButtonNode.addEventListener('click', (event) => {
   event.preventDefault()
-  const newSkillInputName = newSkillInputForm.querySelector('#new_skill')
-  const newSkillName = newSkillInputName.value
-  const newSkillInputDate = newSkillInputForm.querySelector('.input_date')
-  const newSkillDate = newSkillInputDate.value
 
-  //checking the correctness of the entered data
+  const isSkillExists = skillsArray.find(
+    (skill) =>
+      skill.skillName.toLowerCase() ===
+      newSkillInputNode.value.toLowerCase().trim()
+  )
 
-  const errorWindowExist = newSkillInputForm.querySelector('.error_window')
-  const skillNameExist = skillsArr.some((item) => {
-    return item.skillName === newSkillName
-  })
-  if (newSkillName.trim() === '' || newSkillDate === '') {
-    if (errorWindowExist) {
-      errorWindowExist.remove()
-    }
-    createErrorWindow('Заполните все поля!')
-  } else if (skillNameExist) {
-    if (errorWindowExist) {
-      errorWindowExist.remove()
-    }
-    createErrorWindow('Такой навык уже существует!')
+  if (isSkillExists) {
+    removeErrorMessage()
+    createErrorMessage('Данный навык уже существует.')
+  } else if (!newSkillInputNode.value || !dateInputNode.value) {
+    removeErrorMessage()
+    createErrorMessage('Заполните все поля!')
   } else {
-    if (errorWindowExist) {
-      errorWindowExist.remove()
-    }
-    addNewSkill(newSkillName, newSkillDate)
+    removeErrorMessage()
+    addNewSkill(dateInputNode.value, newSkillInputNode.value)
   }
 })
 
-// function to create a new item
+// слушатель на кнопку удаления навыка
+tableNode.addEventListener('click', (event) => {
+  if (event.target.closest('.delete-button')) {
+    const nodeToDelete = event.target.closest('tr')
 
-function addNewSkill(skill, date, id = Date.now().toString()) {
-  const newItemSkillTd = document.createElement('td')
-  newItemSkillTd.textContent = skill
+    // deleteSkill(nodeToDelete)
+    createModal(nodeToDelete)
+  }
+})
 
-  const newItemDateTd = document.createElement('td')
-  newItemDateTd.textContent = date.split('-').reverse().join('.')
+//функция удаления навыка
+function deleteSkill(skillNode) {
+  const skillNodeId = skillNode.getAttribute('id')
+  const IdtoDelete = skillsArray.findIndex((skill) => skill.id === skillNodeId)
+  skillsArray.splice(IdtoDelete, 1)
 
-  const newItemDeleteButtonTd = document.createElement('td')
-  const deleteButton = document.createElement('button')
-  deleteButton.className = 'delete-button'
-  deleteButton.textContent = '✖'
-  newItemDeleteButtonTd.append(deleteButton)
+  deleteModal()
+  skillNode.remove()
+}
 
-  const newItemTr = document.createElement('tr')
-  newItemTr.id = id
-  newItemTr.append(newItemDateTd, newItemSkillTd, newItemDeleteButtonTd)
+//функция добавления ошибки
+function createErrorMessage(text) {
+  const errorMessage = document.createElement('span')
+  errorMessage.className = 'error-message'
+  errorMessage.textContent = text
 
-  const table = document.querySelector('table')
-  table.append(newItemTr)
+  newSkillInputNode.insertAdjacentElement('afterend', errorMessage)
+}
 
-  // to create a new array element
-  const newElemOfSkillsArr = {
+//функция удаления ошибки
+function removeErrorMessage() {
+  const errorMessageNode = document.querySelector('.error-message')
+  if (errorMessageNode) {
+    errorMessageNode.remove()
+  }
+}
+
+//функция добавления нового навыка
+function addNewSkill(date, skill, id = Date.now().toString()) {
+  const newSkilltr = document.createElement('tr')
+  newSkilltr.id = id
+
+  const newSkilltd_1 = document.createElement('td')
+  const newSkilltd_2 = document.createElement('td')
+  const newSkilltd_3 = document.createElement('td')
+
+  newSkilltd_1.textContent = date.split('-').reverse().join('.')
+  newSkilltd_2.textContent = skill.trim()
+
+  const deleteSkillButton = document.createElement('button')
+  deleteSkillButton.className = 'delete-button'
+  deleteSkillButton.textContent = '✖'
+
+  newSkilltd_3.append(deleteSkillButton)
+  newSkilltr.append(newSkilltd_1, newSkilltd_2, newSkilltd_3)
+  tableNode.append(newSkilltr)
+
+  skillsArray.push({
     id: id,
     skillDate: date,
-    skillName: skill,
-  }
-  skillsArr.push(newElemOfSkillsArr)
+    skillName: skill.trim(),
+  })
 }
 
-// function to create a error window
+//функция добавления модального окна
+function createModal(skillNode) {
+  const modal = document.createElement('div')
+  modal.className = 'modal'
+  modal.insertAdjacentHTML(
+    'afterbegin',
+    `<div class="modal-back">
+  <div class="modal-window">
+    <p class="modal-question">Вы действительно хотите удалить навык?</p>
+    <div class="modal-buttons">
+      <button class="confirm">Да</button>
+      <button class="cancel">Отмена</button>
+    </div>
+  </div>
+</div>`
+  )
 
-const createErrorWindow = (text) => {
-  const errorWindow = document.createElement('span')
-  errorWindow.className = 'error_window'
-  errorWindow.textContent = text
+  containerNode.append(modal)
+  //слушатели на кнопки модального окна
+  const confirmButtonNode = document.querySelector('.confirm')
+  const cancelButtonNode = document.querySelector('.cancel')
 
-  const addItemButton = newSkillInputForm.querySelector('button')
-  addItemButton.insertAdjacentElement('beforebegin', errorWindow)
+  confirmButtonNode.addEventListener('click', () => deleteSkill(skillNode))
+
+  cancelButtonNode.addEventListener('click', () => deleteModal())
 }
 
-// addEventListener to delete buttons
-
-const buttonToDeleteSkill = document.querySelector('.delete-button')
-const table = document.querySelector('table')
-table.addEventListener('click', (event) => {
-  const { target } = event
-  const targetIsDeleteButton = target.className.includes('delete-button')
-  if (targetIsDeleteButton) {
-    const skillToDelete = target.closest('tr')
-    skillToDelete.remove()
-
-    // to delete an array element
-    const indexToDelete = skillsArr.findIndex(
-      (skill) => skill.id === skillToDelete.id
-    )
-    skillsArr.splice(indexToDelete, 1)
-  }
-})
+//функция удаления модального окна
+function deleteModal() {
+  const modalNode = document.querySelector('.modal')
+  modalNode.remove()
+}
